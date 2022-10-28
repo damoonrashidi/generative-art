8e76a3effdb7cba1ca2bdba93b368aa3c9d335c7
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-10-28 16:03:35 +0200

    Add piet

--- src/bin/piet.rs
+++ src/bin/piet.rs
@@ -10,15 +10,15 @@ fn main() {
 
     let mut rng = thread_rng();
 
-    for _ in 0..10 {
+    for split in 0..10 {
         for i in (0..rects.len()).rev() {
             if let Some(rect) = rects.get(i) {
                 if rng.gen_bool(0.3) {
                     let (mut a, mut b) = subdivide(rect);
                     rects.remove(i);
 
-                    a.set_color(random_color());
-                    b.set_color(random_color());
+                    a.set_color(random_color(split));
+                    b.set_color(random_color(split));
                     rects.push(a);
                     rects.push(b);
                 }
@@ -34,34 +34,42 @@ fn main() {
 }
 
 fn subdivide(rect: &Rectangle) -> (Rectangle, Rectangle) {
+    let padding = 8.;
+
     let mut rng = thread_rng();
     let split_horizontally = rng.gen_bool(0.5);
 
     if split_horizontally {
-        let a = Rectangle::new(rect.x, rect.y, rect.width / 2., rect.height);
+        let a = Rectangle::new(rect.x, rect.y, rect.width / 2. - padding, rect.height);
         let b = Rectangle::new(
-            rect.x + rect.width / 2.,
+            rect.x + rect.width / 2. + padding,
             rect.y,
-            rect.width / 2.,
+            rect.width / 2. - padding,
             rect.height,
         );
 
         return (a, b);
     }
 
-    let a = Rectangle::new(rect.x, rect.y, rect.width, rect.height - rect.height / 2.);
+    let a = Rectangle::new(
+        rect.x,
+        rect.y,
+        rect.width,
+        rect.height - rect.height / 2. - padding,
+    );
     let b = Rectangle::new(
         rect.x,
-        rect.y + rect.height / 2.,
+        rect.y + rect.height / 2. + padding,
         rect.width,
-        rect.height / 2.,
+        rect.height / 2. - padding,
     );
 
     (a, b)
 }
 
-fn random_color() -> Color {
+fn random_color(split: u8) -> Color {
     let mut rng = thread_rng();
     let hue: u16 = rng.gen_range(0..70);
-    Color::HSLa((hue, 50., 50., 1.))
+    let saturation = 0.1 * split as f64;
+    Color::HSLa((hue, saturation, 50., 1.))
 }
